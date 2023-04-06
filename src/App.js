97836537a0209f1c12a0e253482a30ba2f4accb8
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-06 08:34:32 +0300

    Update app routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
