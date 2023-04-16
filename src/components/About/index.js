625ba9b4bce829358ea493476d2f0a4f11b2247c
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-16 06:30:42 +0300

    Worked on About page

---
+++ src/components/About/index.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import './index.scss';
+import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedText';
+import resume from '../../assets/Abdimulhin Adan.pdf';
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          I&apos;m an ambitious Front-End Developer who&apos;s passionate about
+          coding and looking for a role in the established IT company with
+          the opportunity to work with the latest technologies on
+          challenging and diverse projects.
+        </p>
+        <p>
+          I&apos;m from Kenya but living in Nairobi. I&apos;m a
+          freelancer who loves building cool UI effects, dynamic webpages
+          with amazing animations and creating intuitive, dynamic user
+          experiences. I also love to reach out to new opportunities
+          accross the globe that will enhance my skills and get the
+          opporutinity to work with other programmers as well
+        </p>
+        <p>
+          If I could say one of the most interesting things about me, it&apos;s
+          the fact that I&apos;m always ready for any challenges that comes to
+          my way. Here&apos;s my Resume below for more details.
+        </p>
+        <Link to={resume} className="flat-button">DOWNLOAD RESUME</Link>
+      </div>
+    </div>
+  );
+};
+
+export default About;
