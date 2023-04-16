6d33518a3f9413a0ba0e738cd1499184c8d6095c
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-16 17:33:24 +0300

    Worked on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedText';
+import Logo from './logoAnimation/Logo';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -26,7 +27,21 @@ const Home = () => {
           <span className={`${letterClass} _15`}>&apos;</span>
           <span className={`${letterClass} _14`}>m</span>
           {' '}
-          <span className="letterName">A</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="home-logo"
+            viewBox="0 0 171 210"
+          >
+            <path
+              className="lower-logo-color fill__color home-logo animate-logo-1"
+              d="M171 209.194C159.166 128.482 123.848 68.1928 79.4896 55.1237C39.9765 68.9721 10.4845 132.104 0 210C24.4381 165.9 53.1053 124.32 80.4167 105.975C115.428 124.605 139.927 163.164 171 209.194L171 209.194Z"
+            />
+            <path
+              d="M12.2177 101.884C32.1592 73.1578 55.5534 46.0733 77.8409 34.1237C106.411 46.259 126.403 71.3757 151.759 101.358C142.102 48.7847 113.282 9.51343 77.0849 1C44.8404 10.0195 20.7735 51.1438 12.2177 101.883V101.884Z"
+              fill="#797878"
+              className="animate-logo-2"
+            />
+          </svg>
           <AnimatedLetters
             letterClass={letterClass}
             strArray={nameAttar}
@@ -44,6 +59,7 @@ const Home = () => {
         <h2>Fullstack Developer</h2>
         <Link to="/contact" className="flat-button">CONTACT ME!</Link>
       </div>
+      <Logo />
     </div>
   );
 };
