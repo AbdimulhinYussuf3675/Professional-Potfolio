4c5dcc521552dd4397c801bb7d64f67a51eb3318
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-19 12:07:06 +0300

    SOlve linters

--- src/components/AnimatedText/index.js
+++ src/components/AnimatedText/index.js
@@ -1,10 +1,9 @@
-/* eslint-disable react/no-array-index-key */
 import './index.scss';
 
 const AnimatedLetters = ({ letterClass, strArray, idx }) => (
   <span>
     {strArray.map((char, i) => (
-      <span key={char + i} className={`${letterClass} _${i + idx}`}>
+      <span key={char.id} className={`${letterClass} _${i + idx}`}>
         {char}
       </span>
     ))}
